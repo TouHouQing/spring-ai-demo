d6bbd32ca88cac59dfb9b78469c69029db677082
touhouqing <qinghaoyang@foxmail.com>
2025-08-19 10:33:28 +0800

    Initialize chat memory

--- src/main/java/com/touhouqing/springaidemo/controller/ChatController.java
+++ src/main/java/com/touhouqing/springaidemo/controller/ChatController.java
@@ -6,16 +6,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
+import static org.springframework.ai.chat.memory.ChatMemory.CONVERSATION_ID;
+
 @RequiredArgsConstructor
 @RestController
 public class ChatController {
 
     private final ChatClient chatClient;
 
-    @GetMapping(value = "/chat", produces = "text/event-stream;charset=utf-8")
-    public Flux<String> chat(String message) {
+    @GetMapping(value = "/chat", produces = "text/html;charset=utf-8")
+    public Flux<String> chat(String message, String id) {
         return chatClient.prompt()
                 .user(message)
+                .advisors(a -> a.param(CONVERSATION_ID, id))
                 .stream()
                 .content();
     }
